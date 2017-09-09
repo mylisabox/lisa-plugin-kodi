b2ba0ec5955eea7cc6bc7ba7d74e0cdef6a1be46
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-09-09 17:58:52 +0200

    ignore not supported commands

--- index.js
+++ index.js
@@ -4,59 +4,61 @@ const Plugin = require('lisa-plugin')
 
 module.exports = class KodiPlugin extends Plugin {
 
-    /**
-     * Initialisation of your plugin
-     * Called once, when plugin is loaded
-     * @returns Promise
-     */
-    init() {
-        return super.init()
-    }
+  /**
+   * Initialisation of your plugin
+   * Called once, when plugin is loaded
+   * @returns Promise
+   */
+  init() {
+    return super.init()
+  }
 
-    /**
-     * Called when
-     * @param action to execute
-     * @param infos context of the action
-     * @return Promise
-     */
-    interact(action, infos) {
-      let room = infos.fields.room || infos.context.room
-        if (room) {
-            room = room.id
-        }
-        let episode = infos.fields.episode
-        if (episode) {
-            episode = ("0" + episode).slice(-2)
-        }
-        switch (action) {
-            case "MUTE_VOLUME":
-                return this.drivers.kodi.mute(room)
-            case "UNMUTE_VOLUME":
-                return this.drivers.kodi.unmute(room)
-            case "SET_VOLUME":
-                return this.drivers.kodi.setVolume(infos.fields.number, room)
-            case "INCREASE_VOLUME":
-                return this.drivers.kodi.increaseVolume(infos.fields.number, room)
-            case "DECREASE_VOLUME":
-                return this.drivers.kodi.decreaseVolume(infos.fields.number, room)
-            case "PLAY_TV_SHOW":
-                return this.drivers.kodi.playTvShow(infos.fields.show, infos.fields.season, episode, room)
-            case "PLAY_MOVIE":
-                return this.drivers.kodi.playMovie(infos.fields.movie, room)
-            case "PAUSE_MEDIA_CENTER":
-            case "PLAY_MEDIA_CENTER":
-                return this.drivers.kodi.playPause(room)
-            case "STOP_MEDIA_CENTER":
-                return this.drivers.kodi.stop(room)
-        }
+  /**
+   * Called when
+   * @param action to execute
+   * @param infos context of the action
+   * @return Promise
+   */
+  interact(action, infos) {
+    let room = infos.fields.room || infos.context.room
+    if (room) {
+      room = room.id
+    }
+    let episode = infos.fields.episode
+    if (episode) {
+      episode = ("0" + episode).slice(-2)
+    }
+    switch (action) {
+      case "MUTE_VOLUME":
+        return this.drivers.kodi.mute(room)
+      case "UNMUTE_VOLUME":
+        return this.drivers.kodi.unmute(room)
+      case "SET_VOLUME":
+        return this.drivers.kodi.setVolume(infos.fields.number, room)
+      case "INCREASE_VOLUME":
+        return this.drivers.kodi.increaseVolume(infos.fields.number, room)
+      case "DECREASE_VOLUME":
+        return this.drivers.kodi.decreaseVolume(infos.fields.number, room)
+      case "PLAY_TV_SHOW":
+        return this.drivers.kodi.playTvShow(infos.fields.show, infos.fields.season, episode, room)
+      case "PLAY_MOVIE":
+        return this.drivers.kodi.playMovie(infos.fields.movie, room)
+      case "PAUSE_MEDIA_CENTER":
+      case "PLAY_MEDIA_CENTER":
+        return this.drivers.kodi.playPause(room)
+      case "STOP_MEDIA_CENTER":
+        return this.drivers.kodi.stop(room)
+      default:
         return Promise.resolve()
     }
+    return Promise.resolve()
+  }
 
-    constructor(app) {
-        super(app, {
-            config: require('./config'),
-            drivers: require('./drivers'),
-            pkg: require('./package')
-        })
-    }
+  constructor(app) {
+    super(app, {
+      config: require('./config'),
+      drivers: require('./drivers'),
+      pkg: require('./package')
+    })
+  }
 }
